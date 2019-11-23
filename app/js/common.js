917074d88041ac78f07b77e1df7a751a7507edfd
Godfather <jurijs_kolesnikovs@inbox.lv>
2019-11-24 01:09:26 +0700

    Added like range and dynamic value

--- app/js/common.js
+++ app/js/common.js
@@ -8,4 +8,26 @@ $(function() {
       }, 200)
     }
   })*/
+  var progressValues = {
+    value: 126,
+    max: 1000
+    /* value: 827,
+    max: 1000 */
+  }
+
+  var percentage = progressValues.value / progressValues.max
+
+  var calculatedWidth = (percentage * 80)
+
+  var progress = $('.js-progress')
+  progress.css('width', calculatedWidth)
+
+  $('.js-like-card-active-range').css('width', ($('.js-like-card-range').attr('value') / 100) + '%')
+  $('.js-like-card-amount').text($('.js-like-card-range').attr('value'))
+  /* Like card */
+  $('.js-like-card-range').on('input', function(e) {
+    $('.js-like-card-amount').text(e.target.value)
+    var calculatedWidth = e.target.value / 100
+    $('.js-like-card-active-range').css('width', calculatedWidth + '%')
+  })
 });
